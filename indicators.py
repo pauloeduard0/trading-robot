a2b8dcc913a93a7a784b7c511fa9d18627e45a24
Paulo Eduardo <pauloedu.rocha@hotmail.com>
2021-08-20 23:42:16 -0300

    indicators new

--- indicators.py
+++ indicators.py
@@ -1,6 +1,6 @@
-import pandas_ta as ta
 import MetaTrader5 as mt5
 import pandas as pd
+import pandas_ta as ta
 import time
 from datetime import datetime
 import pytz
@@ -30,5 +30,11 @@ ok = mt5.symbol_select(active, True)
 if not ok:
     print("Asset addition failed ", active)
     mt5.shutdown()
+
+d = mt5.copy_rates_from_pos(active, mt5.TIMEFRAME_D1, 0, 40)
+d = pd.DataFrame(d)
+d = timestamptodate(d)
+
+print(ta.d)
 
 
