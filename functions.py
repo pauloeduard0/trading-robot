e8acd26e9225b47e2df1043bd83be96611527766
Paulo Eduardo <pauloedu.rocha@hotmail.com>
2021-08-30 22:30:32 -0300

    Buy function

---
+++ functions.py
@@ -0,0 +1,82 @@
+import MetaTrader5 as mt5
+import pandas as pd
+import pandas_ta as ta
+import time
+from datetime import datetime
+import pytz
+import cufflinks as cf
+import plotly
+import matplotlib.pyplot as plt
+
+
+def timestamptodate(df):
+    df['time'] = pd.to_datetime(df['time'], unit='s')
+    return df
+
+
+def timestamptodate_ticks(df):
+    df['time'] = pd.to_datetime(df['time'], unit='s')
+    df['time_msc'] = pd.to_datetime(df['time_msc'], unit='ms')
+    return df
+
+
+def buy_market(active, tp, sl, lot, desc="Buy Market"):
+
+    point = mt5.symbol_info(active).point
+    price = mt5.symbol_info_tick(active).ask
+    deviation = 5
+
+    if tp != 0:
+        TP = price + tp
+    else:
+        TP = 0
+
+    if sl != 0:
+        SL = price = sl
+    else:
+        SL = 0
+
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": active,
+        "volume": float(lot),
+        "type": mt5.ORDER_TYPE_BUY,
+        "price": price,
+        "sl": float(SL),
+        "tp": float(TP),
+        "deviation": deviation,
+        "magic": 234000,
+        "comment": desc,
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_IOC,
+    }
+
+    result = mt5.order_send(request)
+
+    print("1. order_send(): by {} {} lots at {} with deviation={} points".format(active, lot, price, deviation))
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print("2. order_send failed, retcode={}".format(result.retcode))
+        result_dict = result.asdict()
+        for field in result_dict.keys():
+            print("  {}={}".format(field, result_dict[field]))
+            if field == "request":
+                traderequest_dict = result_dict[field]._asdict()
+                for tradereq_filed in traderequest_dict:
+                    print(" traderesquest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+
+        print("shutfown() and quit")
+    return result
+
+
+if mt5.initialize():
+    print('Login sucess')
+else:
+    print('Login error', mt5.last_error())
+
+active = 'EURUSD'
+
+ok = mt5.symbol_select(active, True)
+
+if not ok:
+    print("Asset addition failed ", active)
+    mt5.shutdown()
