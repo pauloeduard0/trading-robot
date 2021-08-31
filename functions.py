7f55725a2b13b9c545fc32598672fc6f36d3ad74
Paulo Eduardo <pauloedu.rocha@hotmail.com>
2021-08-30 22:42:48 -0300

    Sell function

--- functions.py
+++ functions.py
@@ -21,7 +21,6 @@ def timestamptodate_ticks(df):
 
 
 def buy_market(active, tp, sl, lot, desc="Buy Market"):
-
     point = mt5.symbol_info(active).point
     price = mt5.symbol_info_tick(active).ask
     deviation = 5
@@ -32,7 +31,7 @@ def buy_market(active, tp, sl, lot, desc="Buy Market"):
         TP = 0
 
     if sl != 0:
-        SL = price = sl
+        SL = price - sl
     else:
         SL = 0
 
@@ -41,6 +40,53 @@ def buy_market(active, tp, sl, lot, desc="Buy Market"):
         "symbol": active,
         "volume": float(lot),
         "type": mt5.ORDER_TYPE_BUY,
+        "price": price,
+        "sl": float(SL),
+        "tp": float(TP),
+        "deviation": deviation,
+        "magic": 234000,
+        "comment": desc,
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_IOC,
+    }
+
+    result = mt5.order_send(request)
+
+    print("1. order_send(): by {} {} lots at {} with deviation={} points".format(active, lot, price, deviation))
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print("2. order_send failed, retcode={}".format(result.retcode))
+        result_dict = result.asdict()
+        for field in result_dict.keys():
+            print("  {}={}".format(field, result_dict[field]))
+            if field == "request":
+                traderequest_dict = result_dict[field]._asdict()
+                for tradereq_filed in traderequest_dict:
+                    print(" traderesquest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+
+        print("shutfown() and quit")
+    return result
+
+
+def sell_market(active, tp, sl, lot, desc="Sell Market"):
+    point = mt5.symbol_info(active).point
+    price = mt5.symbol_info_tick(active).bid
+    deviation = 5
+
+    if tp != 0:
+        TP = price - tp
+    else:
+        TP = 0
+
+    if sl != 0:
+        SL = price + sl
+    else:
+        SL = 0
+
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": active,
+        "volume": float(lot),
+        "type": mt5.ORDER_TYPE_SELL,
         "price": price,
         "sl": float(SL),
         "tp": float(TP),
