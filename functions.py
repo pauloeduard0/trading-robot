bd318280cb3f7f8ba8c3faf7acbc35d90fc75c3c
Paulo Eduardo <pauloedu.rocha@hotmail.com>
2021-09-03 19:14:36 -0300

    Buy Limit function

--- functions.py
+++ functions.py
@@ -114,12 +114,60 @@ def sell_market(active, tp, sl, lot, desc="Sell Market"):
     return result
 
 
+def buy_limit(active, entrada, tp, sl, lot, desc="Buy Limit"):
+    point = mt5.symbol_info(active).point
+    price = mt5.symbol_info_tick(active).ask
+    deviation = 5
+
+    if tp != 0:
+        TP = entrada + tp
+    else:
+        TP = 0
+
+    if sl != 0:
+        SL = entrada - sl
+    else:
+        SL = 0
+
+    request = {
+        "action": mt5.TRADE_ACTION_PENDING,
+        "symbol": active,
+        "volume": float(lot),
+        "type": mt5.ORDER_TYPE_BUY_LIMIT,
+        "price": entrada,
+        "sl": float(SL),
+        "tp": float(TP),
+        "deviation": deviation,
+        "magic": 234000,
+        "comment": desc,
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_FOK,
+    }
+
+    result = mt5.order_send(request)
+
+    print("1. order_send(): by {} {} lots at {} with deviation={} points".format(active, lot, price, deviation))
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print("2. order_send failed, retcode={}".format(result.retcode))
+        result_dict = result.asdict()
+        for field in result_dict.keys():
+            print("  {}={}".format(field, result_dict[field]))
+            if field == "request":
+                traderequest_dict = result_dict[field]._asdict()
+                for tradereq_filed in traderequest_dict:
+                    print(" traderesquest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+
+        # print("shutfown() and quit")
+        # mt5.shutdown()
+    return result
+
+
 if mt5.initialize():
     print('Login sucess')
 else:
     print('Login error', mt5.last_error())
 
-active = 'EURUSD'
+active = 'Volatility 75 Index'
 
 ok = mt5.symbol_select(active, True)
 
